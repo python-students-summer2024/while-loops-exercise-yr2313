9c7902601620241b7475e1c8b015bf528228ecbc
bloombar <amos@cs.nyu.edu>
2023-11-01 04:23:19 -0400

    pylinted

--- tests/test_sing_song.py
+++ tests/test_sing_song.py
@@ -1,20 +1,20 @@
 import pytest
 import string
 
+
 class Tests:
+    def test_for_loop_version_output(self, capsys, monkeypatch):
+        """
+        Checks whether the output is correct using a for loop.
+        """
 
-  def test_for_loop_version_output(self, capsys, monkeypatch):
-    """
-    Checks whether the output is correct using a for loop.
-    """
+        from for_loop_version import sing
 
-    from for_loop_version import sing
+        # run the function
+        sing(5)
 
-    # run the function
-    sing(5)
-
-    # expected output
-    expected = """
+        # expected output
+        expected = """
 5 bottles of beer on the wall, 5 bottles of beer.
 Take one down, pass it around, 4 bottles of beer on the wall.
 
@@ -30,73 +30,75 @@ Take one down, pass it around, 1 bottle of beer on the wall.
 1 bottle of beer on the wall, 1 bottle of beer.
 Take it down, pass it around, no more bottles of beer on the wall!
     """.strip()
-    
-    # get the output
-    captured = capsys.readouterr()  # capture print output
-    actual = captured.out.lower().strip()  # remove leading/trailing whitespace
 
-    # remove multiple whitespaces
-    actual = ' '.join(actual.split()).strip()
-    expected = ' '.join(expected.split()).strip()
+        # get the output
+        captured = capsys.readouterr()  # capture print output
+        actual = captured.out.lower().strip()  # remove leading/trailing whitespace
 
-    # remove all punctuation
-    actual = actual.translate(str.maketrans('', '', string.punctuation))
-    expected = expected.translate(str.maketrans('', '', string.punctuation))
+        # remove multiple whitespaces
+        actual = " ".join(actual.split()).strip()
+        expected = " ".join(expected.split()).strip()
 
-    # convert to lowercase
-    actual = actual.lower()
-    expected = expected.lower()
+        # remove all punctuation
+        actual = actual.translate(str.maketrans("", "", string.punctuation))
+        expected = expected.translate(str.maketrans("", "", string.punctuation))
 
-    # make sure the program only asked for input until a valid one was given
-    assert actual.lower() == expected.lower()
+        # convert to lowercase
+        actual = actual.lower()
+        expected = expected.lower()
 
+        # make sure the program only asked for input until a valid one was given
+        assert actual.lower() == expected.lower()
 
-  def test_for_loop_version_range(self, capsys, monkeypatch):
-    """
-    Checks whether the for loop version calls the range function
-    """
+    def test_for_loop_version_range(self, capsys, monkeypatch):
+        """
+        Checks whether the for loop version calls the range function
+        """
 
-    from for_loop_version import sing
+        from for_loop_version import sing
 
-    range_data = {
-      'calls': 0,
-    }
+        range_data = {
+            "calls": 0,
+        }
 
-    # mock the range function
-    def new_range(oldrange, *args):
-      range_data['calls'] += 1
-      if len(args) == 2:
-        range = abs(args[1] - args[0])
-      elif len(args) == 1:
-        range = args[0]
-      else:
-        range = abs(args[1] - args[0])
+        # mock the range function
+        def new_range(oldrange, *args):
+            range_data["calls"] += 1
+            if len(args) == 2:
+                range = abs(args[1] - args[0])
+            elif len(args) == 1:
+                range = args[0]
+            else:
+                range = abs(args[1] - args[0])
 
-      range_data['range'] = range
-      return oldrange(*args)
-    oldrange = range
-    monkeypatch.setattr('builtins.range', lambda *args: new_range(oldrange, *args))
+            range_data["range"] = range
+            return oldrange(*args)
 
-    # run the function
-    num_bottles = 10
-    sing(num_bottles)
+        oldrange = range
+        monkeypatch.setattr("builtins.range", lambda *args: new_range(oldrange, *args))
 
-    # check the range function has been called
-    assert range_data['calls'] >= 1
-    assert range_data['range'] >= num_bottles - 1 # allow for last verse to be outside loop
+        # run the function
+        num_bottles = 10
+        sing(num_bottles)
 
-  def test_while_loop_accumulator_version_output(self, capsys, monkeypatch):
-    """
-    Checks whether the output is correct using a while loop with accumulator
-    """
+        # check the range function has been called
+        assert range_data["calls"] >= 1
+        assert (
+            range_data["range"] >= num_bottles - 1
+        )  # allow for last verse to be outside loop
 
-    from while_loop_accumulator_version import sing
-    
-    # run the function
-    sing(5)
+    def test_while_loop_accumulator_version_output(self, capsys, monkeypatch):
+        """
+        Checks whether the output is correct using a while loop with accumulator
+        """
 
-    # expected output
-    expected = """
+        from while_loop_accumulator_version import sing
+
+        # run the function
+        sing(5)
+
+        # expected output
+        expected = """
 5 bottles of beer on the wall, 5 bottles of beer.
 Take one down, pass it around, 4 bottles of beer on the wall.
 
@@ -112,39 +114,38 @@ Take one down, pass it around, 1 bottle of beer on the wall.
 1 bottle of beer on the wall, 1 bottle of beer.
 Take it down, pass it around, no more bottles of beer on the wall!
     """.strip()
-    
-    # get the output
-    captured = capsys.readouterr()  # capture print output
-    actual = captured.out.lower().strip()  # remove leading/trailing whitespace
 
-    # remove multiple whitespaces
-    actual = ' '.join(actual.split()).strip()
-    expected = ' '.join(expected.split()).strip()
+        # get the output
+        captured = capsys.readouterr()  # capture print output
+        actual = captured.out.lower().strip()  # remove leading/trailing whitespace
 
-    # remove all punctuation
-    actual = actual.translate(str.maketrans('', '', string.punctuation))
-    expected = expected.translate(str.maketrans('', '', string.punctuation))
+        # remove multiple whitespaces
+        actual = " ".join(actual.split()).strip()
+        expected = " ".join(expected.split()).strip()
 
-    # convert to lowercase
-    actual = actual.lower()
-    expected = expected.lower()
-    
-    # make sure the program only asked for input until a valid one was given
-    assert actual.lower() == expected.lower()
+        # remove all punctuation
+        actual = actual.translate(str.maketrans("", "", string.punctuation))
+        expected = expected.translate(str.maketrans("", "", string.punctuation))
 
+        # convert to lowercase
+        actual = actual.lower()
+        expected = expected.lower()
 
-  def test_while_loop_flag_version_output(self, capsys, monkeypatch):
-    """
-    Checks whether the output is correct using a while loop with flag
-    """
+        # make sure the program only asked for input until a valid one was given
+        assert actual.lower() == expected.lower()
 
-    from while_loop_flag_version import sing
-    
-    # run the function
-    sing(5)
+    def test_while_loop_flag_version_output(self, capsys, monkeypatch):
+        """
+        Checks whether the output is correct using a while loop with flag
+        """
 
-    # expected output
-    expected = """
+        from while_loop_flag_version import sing
+
+        # run the function
+        sing(5)
+
+        # expected output
+        expected = """
 5 bottles of beer on the wall, 5 bottles of beer.
 Take one down, pass it around, 4 bottles of beer on the wall.
 
@@ -160,23 +161,22 @@ Take one down, pass it around, 1 bottle of beer on the wall.
 1 bottle of beer on the wall, 1 bottle of beer.
 Take it down, pass it around, no more bottles of beer on the wall!
     """.strip()
-    
-    # get the output
-    captured = capsys.readouterr()  # capture print output
-    actual = captured.out.lower().strip()  # remove leading/trailing whitespace
 
-    # remove multiple whitespaces
-    actual = ' '.join(actual.split()).strip()
-    expected = ' '.join(expected.split()).strip()
+        # get the output
+        captured = capsys.readouterr()  # capture print output
+        actual = captured.out.lower().strip()  # remove leading/trailing whitespace
 
-    # remove all punctuation
-    actual = actual.translate(str.maketrans('', '', string.punctuation))
-    expected = expected.translate(str.maketrans('', '', string.punctuation))
+        # remove multiple whitespaces
+        actual = " ".join(actual.split()).strip()
+        expected = " ".join(expected.split()).strip()
 
-    # convert to lowercase
-    actual = actual.lower()
-    expected = expected.lower()
-    
-    # make sure the program only asked for input until a valid one was given
-    assert actual.lower() == expected.lower()
+        # remove all punctuation
+        actual = actual.translate(str.maketrans("", "", string.punctuation))
+        expected = expected.translate(str.maketrans("", "", string.punctuation))
 
+        # convert to lowercase
+        actual = actual.lower()
+        expected = expected.lower()
+
+        # make sure the program only asked for input until a valid one was given
+        assert actual.lower() == expected.lower()
